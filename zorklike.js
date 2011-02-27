85dc1e4dd3cb2978616ee4fc6071e933d6d1880d
Wilfred Hughes <me@wilfred.me.uk>
2011-02-27 23:54:03 +0000

    initial commit of prototype

---
+++ zorklike.js
@@ -0,0 +1,146 @@
+zorklike = {}
+
+zorklike.location = new StartLocation();
+
+zorklike.init = function() {
+	$('#shell').terminal(zorklike.terminal.main, zorklike.configuration);
+}
+
+zorklike.terminal = {}
+
+zorklike.terminal.main = function(input, terminal) {
+	var command = $.trim(input);
+	
+	if (command !== '') {
+		var argv = command.split(' ');
+	
+		if (argv[0] in zorklike.commands) {
+			// call command
+			var zorklikeCommand = zorklike.commands[argv[0]];
+			zorklikeCommand(argv.slice(1), terminal);
+		} else {
+			terminal.echo("I don't know how to do that.");
+		}
+	}
+
+	// blank line trailer
+	zorklike.terminal.write('\n', terminal);
+
+	zorklike.terminal.scroll();
+}
+
+zorklike.commands = {}
+
+zorklike.commands.help = function(arguments, terminal) {
+	if (arguments.length === 0) {
+		zorklike.terminal.write("Available commands are: help, look, go. Type 'help <command>' for specific information.",
+					 terminal);
+	} else {
+		if (arguments[0] === 'look') {
+			zorklike.terminal.write("look <direction or item>: describes direction or visible item. Common directions are up, down, forward and backward.",
+						 terminal);
+		} else if (arguments[0] === 'go') {
+			zorklike.terminal.write("go <location>: moves to specified location.",
+						 terminal);
+		} else if (arguments[0] === 'help') {
+			zorklike.terminal.write("help <command>: gives a description of that command.",
+						 terminal);
+		} else {
+			zorklike.terminal.write("No help for that command.",
+						 terminal);
+		}
+	}
+}
+
+zorklike.commands.look = function(arguments, terminal) {
+	if (arguments.length === 0) {
+		zorklike.terminal.write("Which direction you want to look in?",
+					 terminal);
+	} else {
+		var direction = arguments[0];
+		zorklike.terminal.write(
+			zorklike.location.look(direction),
+			terminal);
+	}
+
+}
+
+zorklike.commands.go = function(arguments, terminal) {
+	if (arguments.length === 0) {
+		zorklike.terminal.write("You need to pick a direction to go in.",
+					 terminal);
+	} else {
+		var direction = arguments[0];
+
+		try {
+			zorklike.location = zorklike.location.go(direction);
+			zorklike.terminal.write(
+				zorklike.location.description,
+				terminal);
+
+		} catch (reasonWhyNot) {
+			zorklike.terminal.write(reasonWhyNot, terminal);
+		}
+	}
+
+}
+
+
+zorklike.terminal.write = function(string, terminal) {
+	// write a string to the terminal, breaking long strings on word boundaries.
+	
+	if (string.length <= terminal.cols()) {
+		terminal.echo(string);
+	} else {
+		words = string.split(' ');
+
+		var shortString = ""
+		for (var i=0; i < words.length; i++) {
+			var currentWord = words[i];
+			if (shortString.length + 1 + currentWord.length > terminal.cols()) {
+				// we can't add any more to this line without overflowing.
+
+				// check for stupidly long words, and break them with hyphenation
+				if (currentWord.length > terminal.cols()) {
+					var charsRemaining = terminal.cols() - shortString.length;
+
+					if (shortString === '') {
+						shortString = currentWord.substring(0, charsRemaining - 1) + '-';
+						
+						// save remaining characters of this long word
+						words[i] = currentWord.substring(charsRemaining - 1);
+					} else {
+						shortString += ' ' + currentWord.substring(0, charsRemaining - 2) + '-';
+						// save remaining characters of this long word
+						words[i] = currentWord.substring(charsRemaining - 2);
+
+					}
+
+				}
+
+				// write the truncated string, then recurse on the remainder
+				terminal.echo(shortString);
+				return zorklike.terminal.write(words.slice(i).join(' '), terminal);
+			} else {
+				if (shortString === '') {
+					shortString = words[i];
+				} else {
+					shortString += ' ' + words[i];
+				}
+			}
+		}
+	}
+}
+
+zorklike.terminal.scroll = function() {
+	var bottom = $('#shell').offset().top + $('#shell').height();
+	window.scrollBy(0, bottom);
+}
+
+zorklike.configuration = {greetings: zorklike.location.description +
+			  "\n\nAvailable commands are: help, look, go.\n",
+			  exit: false};
+
+$(function() {
+	zorklike.init();
+});
---
+++ zorklike_locations.js
@@ -0,0 +1,126 @@
+function Location() {
+	this.visible = {};
+	this.description = "";
+}
+
+Location.prototype.look = function(thing) {
+	if (thing in this.visible) {
+		return this.visible[thing];
+	} else {
+		return "You can't see anything there."
+	}
+}
+
+Location.prototype.go = function(direction) {
+	throw "You can't go in that direction here."
+}
+
+function StartLocation() {
+	this.visible = {up: 'You see the top of text interface, with three tempting links above it.',
+			down: 'You can only see the bottom of the ' + getBrowserName() + ' window.',
+			forward: 'In front of you, you see some handcrafted HTML with a curious text interface. Looking more closely, you spot a trapdoor beyond.'};
+
+	// we have to manually add line breaks as this is shown on first load
+	this.description = "You find yourself on a curiously designed website with what looks like\nan interface influenced by text adventure games.";
+}
+
+StartLocation.prototype = new Location;
+
+StartLocation.prototype.go = function(direction) {
+	if (direction === "up") {
+		throw "Nothing above you to go to.";
+	} else if (direction === "down") {
+		throw "The ground looks pretty solid, you can't go down here.";
+	} else if (direction === "forward") {
+		return new Meadow();
+	} else {
+		return Location.prototype.go(direction);
+	}
+}
+
+function Meadow() {
+	// set noticeboard message from twitter
+	var self = this;
+	$.getJSON('http://twitter.com/status/user_timeline/dontYetKnow.json?count=1&callback=?',
+		  function(data, result) {
+			  self.visible.noticeboard = 'The noticeboard reads "' + data[0].text + '"';
+		  });
+
+	this.visible = {up: 'The sky looks blue, with a threat of cloud computing later.',
+			down: 'You are standing in some overgrown grass.',
+			forward: 'You see a hillside not too far away, though the ground looks somewhat rocky.',
+			backward: 'A trapdoor is set into the group behind you.',
+			noticeboard: 'The noticeboard looks blank today.'};
+
+	this.description = "You step through the trapdoor, and blink as your eyes adjust to the bright sunlight. You are standing in a meadow, with a bird cawing in the distance. A noticeboard is here.";
+}
+
+Meadow.prototype = new Location;
+
+Meadow.prototype.go = function(direction) {
+	if (direction === "forward") {
+		return new Hillside();
+	} else if (direction === "backward") {
+		return new StartLocation();
+	} else {
+		return Location.prototype.go(direction);
+	}
+}
+
+function Hillside() {
+	this.visible = {up: 'The sky is still blue, to be honest.',
+			down: 'You are standing on rocky ground, on a slight incline..',
+			forward: 'The hillside stretches as far as you can see.',
+			backward: 'Looking back, you see the meadow you came from.'};
+
+	this.description = "You stride onwards to the hillside, slowing down as the path gets steepier and rockier. The whole area has an unfinished feel to it, somehow.";
+}
+
+Hillside.prototype = new Location;
+
+Hillside.prototype.go = function(direction) {
+	if (direction === "backward") {
+		return new Meadow();
+	} else {
+		return Location.prototype.go(direction);
+	}
+}
+
+function getBrowserName() {
+	// based on http://www.javascripter.net/faq/browsern.htm
+	var userAgent = navigator.userAgent;
+	var browserName = navigator.appName;
+
+	// In MSIE, the true version is after "MSIE" in userAgent
+	if (userAgent.indexOf("MSIE") != -1) {
+		browserName = "Microsoft Internet Explorer";
+	}
+	// In Opera, the true version is after "Opera" 
+	else if (userAgent.indexOf("Opera") != -1) {
+		browserName = "Opera";
+	}
+	// In Chrome, the true version is after "Chrome" 
+	else if (userAgent.indexOf("Chrome") != -1) {
+		browserName = "Chrome";
+	}
+	// In Safari, the true version is after "Safari" 
+	else if (userAgent.indexOf("Safari")!=-1) {
+		browserName = "Safari";
+	}
+	// In Firefox, the true version is after "Firefox" 
+	else if (userAgent.indexOf("Firefox") != -1) {
+		browserName = "Firefox";
+	}
+	// In most other browsers, "name/version" is at the end of userAgent 
+	else if (userAgent.lastIndexOf(' ') + 1 < userAgent.lastIndexOf('/')) {
+		var nameOffset = userAgent.lastIndexOf(' ') + 1;
+		var verOffset = userAgent.lastIndexOf('/');
+		browserName = userAgent.substring(nameOffset, verOffset);
+
+		if (browserName.toLowerCase() == browserName.toUpperCase()) {
+			browserName = navigator.appName;
+		}
+	}
+
+	return browserName;
+}
