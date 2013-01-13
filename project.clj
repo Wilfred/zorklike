d89ae994ef20803477d0bb804309a277353de42f
Wilfred Hughes <me@wilfred.me.uk>
2013-01-13 01:12:36 +0000

    specifying compiled JS path

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :plugins [[lein-cljsbuild "0.2.10"]]
     :cljsbuild {
     :builds [{:source-path "src"
-              :compiler {;;:output-to "resources/public/js/main.js"
+              :compiler {:output-to "target/main.js"
                          :optimizations :advanced
-                         :pretty-print true}}]})
+                         :pretty-print false}}]})
