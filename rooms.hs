ef2561a4235cd9ac7763565c04d2cb3cff82ee93
Wilfred Hughes <me@wilfred.me.uk>
2013-01-20 20:09:16 +0000

    implementing basic directions

--- rooms.hs
+++ rooms.hs
@@ -1,10 +1,18 @@
 import Language.Fay.Prelude
 import Language.Fay.FFI
 
-data Place = Place { description :: String } deriving (Show)
+data Place = Place { description :: String, directions :: [String]} 
+           deriving (Show)
 
 start :: Place
-start = Place {description="You find yourself on a curiously designed website with what looks like\nan interface influenced by text adventure games."}
+start = Place {description="You find yourself on a curiously designed website with what looks like\nan interface influenced by text adventure games.",
+               directions=["up", "down", "forward"]}
+        
+describePlace :: Place -> String
+describePlace place = description place ++ "\n\nDirections here: " ++ placeDirections
+  where
+    join sep list = concat $ intersperse sep list
+    placeDirections = join ", " $ directions place
 
 clearScrollback :: Fay ()
 clearScrollback = ffi "jQuery('.scrollback').text('')"
@@ -17,4 +25,4 @@ console_log = ffi "console.log(%1)"
 
 main = do
   clearScrollback
-  writeScrollback $ description start
+  writeScrollback $ describePlace start
