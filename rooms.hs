b181fbab06888258aa5357dcf13ebcbd0df77fc3
Wilfred Hughes <me@wilfred.me.uk>
2013-01-28 23:38:20 +0000

    implementing a skeleton look command

--- rooms.hs
+++ rooms.hs
@@ -1,11 +1,19 @@
 import Prelude
 import FFI
 
-data Place = Place { description :: String, directions :: [String]} 
+isPrefixOf              :: (Eq a) => [a] -> [a] -> Bool
+isPrefixOf [] _         =  True
+isPrefixOf _  []        =  False
+isPrefixOf (x:xs) (y:ys)=  x == y && isPrefixOf xs ys
+
+data Place = Place { description :: String,
+                     arrivalMessage :: String,
+                     directions :: [String]} 
            deriving (Show)
 
 start :: Place
-start = Place {description="You find yourself on a curiously designed website. It's definitely HTML, but something is... different. You hear movement in the distance, and there's a smell of burning Haskell in the air.\n\nAvailable commands: go",
+start = Place {arrivalMessage="You find yourself on a curiously designed website. It's definitely HTML, but something is... different. You hear movement in the distance, and there's a smell of burning Haskell in the air.\n\nAvailable commands: go, look",
+               description="The DOM nodes are spaced widely here, but it's thicker ahead. You feel nervous, but decide to learn more of this strange land and press onwards. Am I right?",
                directions=["up", "down", "forward"]}
         
 describePlace :: Place -> String
@@ -35,22 +43,26 @@ clearInput = ffi "jQuery('#prompt').val('')"
 getInput :: Fay String
 getInput = ffi "jQuery('#prompt').val()"
 
-data Command = Go
+data Command = Go | Look
 
 parseInput :: String -> Maybe Command
-parseInput ('g':'o':xs) = Just Go
-parseInput _ = Nothing
+parseInput input
+  | "go" `isPrefixOf` input = Just Go
+  | "look" `isPrefixOf` input = Just Look
+  | otherwise = Nothing
 
 respondToCommand = do
   input <- getInput
   let command = parseInput input
   case command of
     Just Go -> writeScrollback "Go!"
+    -- todo: store the current location
+    Just Look -> writeScrollback $ description start
     _ -> writeScrollback "I don't know how to do that."
   clearInput
 
 main = do
   clearScrollback
-  writeScrollback $ describePlace start
+  writeScrollback $ arrivalMessage start
   runOnEnter respondToCommand
   
